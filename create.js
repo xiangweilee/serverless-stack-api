9b4a1207b2993bc0154b59ba6c70d80f3e89c1ba
Lee Xiang Wei <xiangweilee@seekasia.com>
2019-12-22 15:54:34 +0800

    Add a create note API

---
+++ create.js
@@ -0,0 +1,34 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Congito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from the request body
+    // - 'attachment': parsed from the request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+};
